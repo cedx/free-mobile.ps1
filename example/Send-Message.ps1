7dca54d7051403fc02afca4de38ced5fd034da05
Cédric Belin <cedx@outlook.com>
2025-11-02 22:25:56 +0100

    Code formatting [skip ci]

--- example/Send-Message.ps1
+++ example/Send-Message.ps1
@@ -6,4 +6,5 @@ Import-Module FreeMobile
 
 $password = ConvertTo-SecureString "Your API key" -AsPlainText
 Send-FreeMobileMessage "Hello World from PowerShell!" -UserName "Your account identifier" -Password $password
+
 Write-Output "The message was sent successfully."
