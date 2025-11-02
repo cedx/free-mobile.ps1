a3f39f13eca1253b39f9e4151e2d262bee85818e
Cédric Belin <cedx@outlook.com>
2025-11-02 19:52:38 +0100

    Add the tests of the `Main` module

---
+++ test/Main.Tests.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+	Tests the features of the `Main` module.
+#>
+Describe "Main" {
+	BeforeAll {
+		Import-Module ./FreeMobile.psd1
+	}
+
+	Context "Send-Message" {
+		It "should throw an exception if a network error occurred" {
+			$password = ConvertTo-SecureString "secret" -AsPlainText
+			{ "Hello World!" | Send-FreeMobileMessage -UserName "anonymous" -Password $password -Uri "http://localhost:666" } | Should -Throw
+		}
+
+		It "should throw a `HttpRequestException` if the credentials are invalid" {
+			$password = ConvertTo-SecureString "secret" -AsPlainText
+			{ "Hello World!" | Send-FreeMobileMessage -UserName "anonymous" -Password $password } | Should -Throw
+		}
+
+		It "should send SMS messages if the credentials are valid" {
+			$password = ConvertTo-SecureString $Env:FREEMOBILE_API_KEY -AsPlainText
+			{ "Hello Cédric, from PowerShell!" | Send-FreeMobileMessage -UserName $Env:FREEMOBILE_ACCOUNT -Password $password } | Should -Not -Throw
+		}
+	}
+}
