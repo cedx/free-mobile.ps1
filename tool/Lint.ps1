0e3c979a302676674471de080c5533e69f939ce5
Cédric Belin <cedx@outlook.com>
2025-11-22 23:56:57 +0100

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -2,5 +2,5 @@
 Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
 Invoke-ScriptAnalyzer src -Recurse
-Test-ModuleManifest "$PSScriptRoot/../FreeMobile.psd1" | Out-Null
+Test-ModuleManifest FreeMobile.psd1 | Out-Null
 Invoke-ScriptAnalyzer test -ExcludeRule PSAvoidUsingConvertToSecureStringWithPlainText -Recurse
